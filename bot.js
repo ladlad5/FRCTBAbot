aee21fbed72a41e1f86bb5572a555703a1acd2c9
ladlad5 <gilad.stessel@gmail.com>
2022-04-19 01:13:13 -0400

    adding commands

--- bot.js
+++ bot.js
@@ -1,4 +1,9 @@
-const {Client, Intents} = require('discord.js')
-const {token} = require('./config.json')
-const client = new Client({ intents: [Intents.FLAGS.GUILDS]})
-client.login(token)
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const {Client, Intents} = require('discord.js');
+const {token} = require('./config.json');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS]});
+client.login(token);
+
+const commands = [
+    new SlashCommandBuilder().setName("ping").setDescription("hi sisters")
+]
